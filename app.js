7f9a299155fede661ecb5c7c53b6233050397292
jtvberg <jtvberg@gmail.com>
2021-07-31 13:18:39 -0500

    clean up

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
-var express = require('express'); //Express web server framework
-var request = require('request'); //http Request library
-var querystring = require('querystring'); //node querystring module
-var cookieParser = require('cookie-parser'); //Cookie management
-var keys = require('./keys'); //Imports key variables, consult README
+var express = require('express') //Express web server framework
+var request = require('request') //http Request library
+var querystring = require('querystring') //node querystring module
+var cookieParser = require('cookie-parser') //Cookie management
+var keys = require('./keys') //Imports key variables, consult README
 
 //Generates a random string of characters
-var generateRandomString = function(length) {
-  var text = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+var generateRandomString = function (length) {
+  var text = ''
+  var possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
   for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
+    text += possible.charAt(Math.floor(Math.random() * possible.length))
   }
-  return text;
-};
+  return text
+}
 
-var stateKey = 'spotify_auth_state';
-var app = express();
+var stateKey = 'spotify_auth_state'
+var app = express()
 
-app.use(express.static(__dirname + '/public'))
-   .use(cookieParser());
+app.use(express.static(__dirname + '/public')).use(cookieParser())
 
-app.get('/login', function(req, res) {
-
+app.get('/login', function (req, res) {
   //Generate state key
-  var state = generateRandomString(16);
-  res.cookie(stateKey, state);
+  var state = generateRandomString(16)
+  res.cookie(stateKey, state)
 
   //Authorization request scope
   var scope = 'user-read-private '
@@ -35,101 +34,116 @@ app.get('/login', function(req, res) {
   scope += 'user-read-birthdate '
   scope += 'user-read-recently-played '
   scope += 'playlist-read-collaborative '
-  scope += 'playlist-read-private';
+  scope += 'playlist-read-private'
 
-  res.redirect('https://accounts.spotify.com/authorize?' +
+  res.redirect(
+    'https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
       client_id: keys.client_id,
       scope: scope,
       redirect_uri: keys.redirect_uri,
-      state: state
-    }));
-});
+      state: state,
+    })
+  )
+})
 
-app.get('/callback', function(req, res) {
-
+app.get('/callback', function (req, res) {
   //Request tokens
-  var code = req.query.code || null;
-  var state = req.query.state || null;
-  var storedState = req.cookies ? req.cookies[stateKey] : null;
+  var code = req.query.code || null
+  var state = req.query.state || null
+  var storedState = req.cookies ? req.cookies[stateKey] : null
 
   if (state === null || state !== storedState) {
-    res.redirect('/#' +
+    res.redirect(
+      '/#' +
       querystring.stringify({
-        error: 'state_mismatch'
-      }));
+        error: 'state_mismatch',
+      })
+    )
   } else {
-    res.clearCookie(stateKey);
+    res.clearCookie(stateKey)
     var authOptions = {
       url: 'https://accounts.spotify.com/api/token',
       form: {
         code: code,
         redirect_uri: keys.redirect_uri,
-        grant_type: 'authorization_code'
+        grant_type: 'authorization_code',
       },
       headers: {
-        'Authorization': 'Basic ' + (new Buffer(keys.client_id + ':' + keys.client_secret).toString('base64'))
+        Authorization: 'Basic ' +
+          new Buffer(keys.client_id + ':' + keys.client_secret).toString(
+            'base64'
+          ),
       },
-      json: true
-    };
+      json: true,
+    }
 
-    request.post(authOptions, function(error, response, body) {
+    request.post(authOptions, function (error, response, body) {
       if (!error && response.statusCode === 200) {
-
         var access_token = body.access_token,
-            refresh_token = body.refresh_token;
+          refresh_token = body.refresh_token
 
         var options = {
           url: 'https://api.spotify.com/v1/me',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true
-        };
+          headers: {
+            Authorization: 'Bearer ' + access_token
+          },
+          json: true,
+        }
 
         //Connect to API using token
-        request.get(options, function(error, response, body) {
-          console.log(body);
-        });
+        request.get(options, function (error, response, body) {
+          console.log(body)
+        })
 
         //Pass token to the browser to make requests
-        res.redirect('/#' +
+        res.redirect(
+          '/#' +
           querystring.stringify({
             access_token: access_token,
-            refresh_token: refresh_token
-          }));
+            refresh_token: refresh_token,
+          })
+        )
       } else {
-        res.redirect('/#' +
+        res.redirect(
+          '/#' +
           querystring.stringify({
-            error: 'invalid_token'
-          }));
+            error: 'invalid_token',
+          })
+        )
       }
-    });
+    })
   }
-});
+})
 
-app.get('/refresh_token', function(req, res) {
-
+app.get('/refresh_token', function (req, res) {
   //Request access token using refresh token
-  var refresh_token = req.query.refresh_token;
+  var refresh_token = req.query.refresh_token
   var authOptions = {
     url: 'https://accounts.spotify.com/api/token',
-    headers: { 'Authorization': 'Basic ' + (new Buffer(keys.client_id + ':' + keys.client_secret).toString('base64')) },
+    headers: {
+      Authorization: 'Basic ' +
+        new Buffer(keys.client_id + ':' + keys.client_secret).toString(
+          'base64'
+        ),
+    },
     form: {
       grant_type: 'refresh_token',
-      refresh_token: refresh_token
+      refresh_token: refresh_token,
     },
-    json: true
-  };
+    json: true,
+  }
 
-  request.post(authOptions, function(error, response, body) {
+  request.post(authOptions, function (error, response, body) {
     if (!error && response.statusCode === 200) {
-      var access_token = body.access_token;
+      var access_token = body.access_token
       res.send({
-        'access_token': access_token
-      });
+        access_token: access_token,
+      })
     }
-  });
-});
+  })
+})
 
-console.log('Listening on 8080');
-app.listen(8080);
+console.log('Listening on 8080')
+app.listen(8080)
--- public/api.js
+++ public/api.js
@@ -1,95 +1,96 @@
 //Event listener to execute API call on click
 //Track ids injected for testing if no value entered
 document.getElementById('execute-query').addEventListener('click', function () {
-    var type = document.getElementById('inputGroupSelect01').value;
-    var id = !document.getElementById('spotify-id').value ? '14KlyPJREBjS02AiIXCIqk%2C2LUGAmTUIgFyHPVWmFRzvw' : multiId(document.getElementById('spotify-id').value);
-    if(id.includes(":")) {
-        var ids = id.split(":");
-        id = ids[0];
-        uid = ids[1];
+    var type = document.getElementById('inputGroupSelect01').value
+    var id = !document.getElementById('spotify-id').value ?
+        '14KlyPJREBjS02AiIXCIqk%2C2LUGAmTUIgFyHPVWmFRzvw' :
+        multiId(document.getElementById('spotify-id').value)
+    if (id.includes(':')) {
+        var ids = id.split(':')
+        id = ids[0]
+        uid = ids[1]
     } else {
-        uid = userid;
+        uid = userid
     }
-    runQuery(buildUrl(type, id, uid), type);
-});
+    runQuery(buildUrl(type, id, uid), type)
+})
 //Build the API URL
 function buildUrl(type, id, uid) {
-    var url = "https://api.spotify.com/v1/";
+    var url = 'https://api.spotify.com/v1/'
     switch (type) {
         case 'track':
-            url += `tracks/${id}?market=${country}`;
-            break;
+            url += `tracks/${id}?market=${country}`
+            break
         case 'features':
             url += `audio-features/${id}`
-            break;
+            break
         case 'tracks':
-            url += `tracks?ids=${id}&market=${country}`;
-            break;
+            url += `tracks?ids=${id}&market=${country}`
+            break
         case 'artist':
-            url += `artists/${id}?market=${country}`;
-            break;
+            url += `artists/${id}?market=${country}`
+            break
         case 'album':
-            url += `albums/${id}?market=${country}`;
-            break;
+            url += `albums/${id}?market=${country}`
+            break
         case 'playlists':
-            url += `users/${uid}/playlists`
-            break;
+            url += `users/${uid}/playlists?limit=50`
+            break
         case 'playlistInfo':
             url += `users/${uid}/playlists/${id}`
-            break;
+            break
         case 'playlistTracks':
             url += `users/${uid}/playlists/${id}/tracks`
-            break;
+            break
         case 'libTracks':
             url += `me/tracks`
-            break;
+            break
         case 'libAlbums':
             url += `me/albums`
-            break;
+            break
         default:
-            url += `tracks?ids=${id}&market=${country}`;
-            break;
+            url += `tracks?ids=${id}&market=${country}`
+            break
     }
-    return url;
+    return url
 }
 //Replace commas with HTML code, remove white space
 function multiId(input) {
-    return input.replace(/ /g, "").replace(/,/g, "%2C");
+    return input.replace(/ /g, '').replace(/,/g, '%2C')
 }
 //Hit Spotify API and return data object
 function runQuery(url, type) {
-    console.log(url);
+    console.log(url)
     return fetch(url, {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token,
-        },
-    })
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + token,
+            },
+        })
         .then(function (response) {
-            return response.text();
+            return response.text()
         })
         .then(function (data) {
-            var data_obj = JSON.parse(data);
-            console.log(data_obj);
-            parseObj(data_obj, type);
-            return data_obj;
+            var data_obj = JSON.parse(data)
+            console.log(data_obj)
+            parseObj(data_obj, type)
+            return data_obj
         })
 }
 
-var next, tracks, playlists, playlistTracks, libTracks;
+var next, tracks, playlists, playlistTracks, libTracks
 //Parse data object into handlebars template
 function parseObj(obj, type) {
+    var placeholderHeader = document.getElementById('resultsHeader')
+    var placeholder = document.getElementById('results')
+    var source, template
 
-    var placeholderHeader = document.getElementById('resultsHeader');
-    var placeholder = document.getElementById('results');
-    var source, template;
-    
     switch (type) {
         case 'track':
-            source = $("#track-template").html();
-            template = Handlebars.compile(source);
+            source = $('#track-template').html()
+            template = Handlebars.compile(source)
             placeholder.innerHTML = template({
                 name: obj.name,
                 duration_ms: obj.duration_ms,
@@ -97,18 +98,18 @@ function parseObj(obj, type) {
                 album: obj.album.name,
                 id: obj.id,
                 popularity: obj.popularity,
-                img: obj.album.images[2].url
-            });
-            placeholderHeader.innerHTML = '';
-            tracks = null;
-            playlists = null;
-            playlistTracks = null;
-            libTracks = null;
-            break;
+                img: obj.album.images[2].url,
+            })
+            placeholderHeader.innerHTML = ''
+            tracks = null
+            playlists = null
+            playlistTracks = null
+            libTracks = null
+            break
         case 'features':
-            source = $("#features-template").html();
-            template = Handlebars.compile(source);
-            placeholder.innerHTML = template({ 
+            source = $('#features-template').html()
+            template = Handlebars.compile(source)
+            placeholder.innerHTML = template({
                 acousticness: obj.acousticness,
                 danceability: obj.danceability,
                 duration_ms: obj.duration_ms,
@@ -121,161 +122,173 @@ function parseObj(obj, type) {
                 speechiness: obj.speechiness,
                 tempo: obj.tempo,
                 time_signature: obj.time_signature,
-                valence: obj.valence
-            });
-            placeholderHeader.innerHTML = '';
-            tracks = null;
-            playlists = null;
-            playlistTracks = null;
-            libTracks = null;
+                valence: obj.valence,
+            })
+            placeholderHeader.innerHTML = ''
+            tracks = null
+            playlists = null
+            playlistTracks = null
+            libTracks = null
             break
         case 'tracks':
-        console.log(obj.tracks);
-            if(!tracks) {
-                tracks = obj.tracks;
+            console.log(obj.tracks)
+            if (!tracks) {
+                tracks = obj.tracks
             } else {
-                for (i = 0; i < obj.tracks.length; i++) { 
-                    if(!checkDupe(obj.tracks[i].id)) {
-                        tracks.push(obj.tracks[i]);
+                for (i = 0; i < obj.tracks.length; i++) {
+                    if (!checkDupe(obj.tracks[i].id)) {
+                        tracks.push(obj.tracks[i])
                     }
                 }
             }
-            console.log(tracks);
-            source = $("#tracks-template").html();
-            template = Handlebars.compile(source);
-            placeholder.innerHTML = template({ objects: tracks });
-            placeholderHeader.innerHTML = '';
-            playlists = null;
-            playlistTracks = null;
-            libTracks = null;
+            console.log(tracks)
+            source = $('#tracks-template').html()
+            template = Handlebars.compile(source)
+            placeholder.innerHTML = template({
+                objects: tracks,
+            })
+            placeholderHeader.innerHTML = ''
+            playlists = null
+            playlistTracks = null
+            libTracks = null
             if (obj.next) {
-                runQuery(obj.next, type);
+                runQuery(obj.next, type)
             }
-            break;
+            break
         case 'artist':
-            source = $("#artist-template").html();
-            template = Handlebars.compile(source);
-            placeholder.innerHTML = template({ 
+            source = $('#artist-template').html()
+            template = Handlebars.compile(source)
+            placeholder.innerHTML = template({
                 name: obj.name,
                 genres: obj.genres,
                 popularity: obj.popularity,
-                id: obj.id
-            });
-            placeholderHeader.innerHTML = '';
-            tracks = null;
-            playlists = null;
-            playlistTracks = null;
-            libTracks = null;
-            break;
+                id: obj.id,
+            })
+            placeholderHeader.innerHTML = ''
+            tracks = null
+            playlists = null
+            playlistTracks = null
+            libTracks = null
+            break
         case 'album':
-            source = $("#album-template").html();
-            template = Handlebars.compile(source);
-            placeholder.innerHTML = template({ name: obj.name });
-            placeholderHeader.innerHTML = '';
-            tracks = null;
-            playlists = null;
-            playlistTracks = null;
-            libTracks = null;
-            break;
+            source = $('#album-template').html()
+            template = Handlebars.compile(source)
+            placeholder.innerHTML = template({
+                name: obj.name,
+            })
+            placeholderHeader.innerHTML = ''
+            tracks = null
+            playlists = null
+            playlistTracks = null
+            libTracks = null
+            break
         case 'playlists':
-            source = $("#playlists-template").html();
-            template = Handlebars.compile(source);
-            placeholder.innerHTML = template({ objects: obj.items });
-            placeholderHeader.innerHTML = '';
-            tracks = null;
-            playlistTracks = null;
-            libTracks = null;
-            break;
+            source = $('#playlists-template').html()
+            template = Handlebars.compile(source)
+            placeholder.innerHTML = template({
+                objects: obj.items,
+            })
+            placeholderHeader.innerHTML = ''
+            tracks = null
+            playlistTracks = null
+            libTracks = null
+            break
         case 'playlistInfo':
-            source = $("#playlistInfo-template").html();
-            template = Handlebars.compile(source);
-            placeholderHeader.innerHTML = template({ 
+            source = $('#playlistInfo-template').html()
+            template = Handlebars.compile(source)
+            placeholderHeader.innerHTML = template({
                 name: obj.name,
                 description: obj.description === '' ? 'NA' : obj.description,
                 owner: obj.owner.id,
-                tracks: obj.tracks.total
-            });
-            tracks = null;
-            playlists = null;
-            playlistTracks = null;
-            libTracks = null;
+                tracks: obj.tracks.total,
+            })
+            tracks = null
+            playlists = null
+            playlistTracks = null
+            libTracks = null
             //runQuery(buildUrl('playlistTracks', obj.id), 'playlistTracks');
         case 'playlistTracks':
-            if(!playlistTracks) {
-                playlistTracks = obj.tracks.items;
-                next = obj.tracks.next;
+            if (!playlistTracks) {
+                playlistTracks = obj.tracks.items
+                next = obj.tracks.next
             } else {
-                for (i = 0; i < obj.items.length; i++) { 
-                    playlistTracks.push(obj.items[i]);
+                for (i = 0; i < obj.items.length; i++) {
+                    playlistTracks.push(obj.items[i])
                 }
-                next = obj.next;
+                next = obj.next
             }
-            source = $("#playlistTracks-template").html();
-            template = Handlebars.compile(source);
-            placeholder.innerHTML = template({ objects: playlistTracks });
-            tracks = null;
-            playlists = null;
-            libTracks = null;
+            source = $('#playlistTracks-template').html()
+            template = Handlebars.compile(source)
+            placeholder.innerHTML = template({
+                objects: playlistTracks,
+            })
+            tracks = null
+            playlists = null
+            libTracks = null
             if (next) {
-                runQuery(next, 'playlistTracks');
+                runQuery(next, 'playlistTracks')
             }
-            break;
+            break
         case 'libTracks':
-            if(!libTracks) {
-                libTracks = obj.items;
+            if (!libTracks) {
+                libTracks = obj.items
             } else {
-                for (i = 0; i < obj.items.length; i++) { 
-                    libTracks.push(obj.items[i]);
+                for (i = 0; i < obj.items.length; i++) {
+                    libTracks.push(obj.items[i])
                 }
             }
-            source = $("#libTracks-template").html();
-            template = Handlebars.compile(source);
-            placeholder.innerHTML = template({ objects: libTracks });
-            placeholderHeader.innerHTML = '';
-            tracks = null;
-            playlists = null;
-            playlistTracks = null;
+            source = $('#libTracks-template').html()
+            template = Handlebars.compile(source)
+            placeholder.innerHTML = template({
+                objects: libTracks,
+            })
+            placeholderHeader.innerHTML = ''
+            tracks = null
+            playlists = null
+            playlistTracks = null
             if (obj.next) {
-                runQuery(obj.next, type);
+                runQuery(obj.next, type)
             }
-            break;
-        default: //set to tracks for testing
-;           source = $("#tracks-template").html();
-            template = Handlebars.compile(source);
-            placeholder.innerHTML = template({ objects: tracks });
-            placeholderHeader.innerHTML = '';
-            playlists = null;
-            playlistTracks = null;
-            libTracks = null;
-            break;
+            break
+        default:
+            //set to tracks for testing
+            source = $('#tracks-template').html()
+            template = Handlebars.compile(source)
+            placeholder.innerHTML = template({
+                objects: tracks,
+            })
+            placeholderHeader.innerHTML = ''
+            playlists = null
+            playlistTracks = null
+            libTracks = null
+            break
     }
 }
 //0Zt18U2PgV39wE4xTaXj2f
 var checkDupe = (val) => {
     for (i = 0; i < tracks.length; i++) {
         if (val === tracks[i].id) {
-            return true;
+            return true
         }
-    };
-    return false;
+    }
+    return false
 }
 //Helper to convert milliseconds to mm:ss format
 Handlebars.registerHelper('time', function (millis) {
-    var minutes = Math.floor(millis / 60000);
-    var seconds = ((millis % 60000) / 1000).toFixed(0);
+    var minutes = Math.floor(millis / 60000)
+    var seconds = ((millis % 60000) / 1000).toFixed(0)
     return new Handlebars.SafeString(
-        minutes + ":" + (seconds < 10 ? '0' : '') + seconds
-    );
-});
+        minutes + ':' + (seconds < 10 ? '0' : '') + seconds
+    )
+})
 //Attached to buttons to grab spotify id
 var lookUpId = (val) => {
-    document.getElementById('spotify-id').value = val.value;
+    document.getElementById('spotify-id').value = val.value
 }
 
 var playById = (val) => {
-    document.getElementById('player').src = "https://open.spotify.com/embed?uri=spotify:track:"+val.value;
+    document.getElementById('player').src =
+        'https://open.spotify.com/embed?uri=spotify:track:' + val.value
 }
 
-var getPage = (val) => {
-    
-}
+var getPage = (val) => {}
